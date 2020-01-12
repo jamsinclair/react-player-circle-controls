4666d71cc41cbe4c321ed5b37d8fdf496e6fbaf3
jamsinclair <jamsinclairnz@gmail.com>
2020-01-12 15:30:04 +0900

    Add static storybook files to docs

---
+++ docs/main.a5cc15f32995be72cf50.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1224:function(e,n,t){"use strict";t.r(n);var c=t(99),i=t(400);c.addons.setConfig({theme:Object(i.create)({base:"light",brandTitle:"React Player Circle"})})},401:function(e,n,t){t(402),t(542),t(1224),t(1225),e.exports=t(1227)},461:function(e,n){}},[[401,1,2]]]);
